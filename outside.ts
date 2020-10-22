6520b0f45cb85c229b2ae6cfb2c39846242c94c3
yoni was <yonixw@gmail.com>
2020-10-23 01:49:20 +0300

    no type generated

---
+++ outside.ts
@@ -0,0 +1,7 @@
+import  * as plugin_bundle from './bundle/bundle'
+console.log(plugin_bundle.getDefaultExport())
+
+console.log(require('./bundle/bundle'))
+
+import  * as plugin_code from './src/expose'
+console.log(plugin_code.getDefaultExport()) 
