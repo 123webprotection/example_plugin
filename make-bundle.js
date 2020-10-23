7c074bd5439374d81a86b868d6e96bdfb22b2701
yoni was <yonixw@gmail.com>
2020-10-23 03:06:34 +0300

    Export name from package name

--- make-bundle.js
+++ make-bundle.js
@@ -3,13 +3,15 @@ var browserify = require("browserify");
 var tsify = require("tsify");
 var fs = require("fs");
 
+var package = JSON.parse(require("fs").readFileSync("package.json"))
+
 // https://www.typescriptlang.org/docs/handbook/integrating-with-build-tools.html#browserify
 
 // Windows: https://github.com/PiotrDabkowski/Js2Py/issues/125
 
 // https://github.com/PiotrDabkowski/Js2Py/blob/master/js2py/node_import.py
 
-browserify({debug: true,standalone: "plugin1"})
+browserify({debug: true,standalone: package.name })
     .on('error',console.error.bind(console))
     .add("./src/expose.ts")
     .plugin(tsify)
